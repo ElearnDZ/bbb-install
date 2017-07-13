b857515b75d52b2a63f989e8cf6281e14c2acb29
Richard Siddall <richard.siddall@elirion.net>
2017-07-13 09:33:21 -0400

    Pass '-y' flag from bbb-install to install-ffmpeg.sh, if specified.  Also add latest flags to the usage summary line in bbb-install.

--- install-ffmpeg.sh
+++ install-ffmpeg.sh
@@ -2,7 +2,27 @@
 
 # Script to install FFMPEG from the BBB 1.0 install instructions
 
-sudo apt-get install build-essential git-core checkinstall yasm texi2html libvorbis-dev libx11-dev libvpx-dev libxfixes-dev zlib1g-dev pkg-config netcat libncurses5-dev
+ANSWER=
+
+usage() {
+	echo "install-ffmpeg.sh [-h] [-y]"
+	echo "where:"
+	echo "	-h = Help"
+	echo "	-y = Answer Y to apt-get install"
+	exit 1
+}
+
+while getopts "yh" flag
+do
+	case $flag in
+	y)
+		ANSWER='-y';;
+	h)
+		usage;;
+	esac
+done
+
+sudo apt-get ${ANSWER} install build-essential git-core checkinstall yasm texi2html libvorbis-dev libx11-dev libvpx-dev libxfixes-dev zlib1g-dev pkg-config netcat libncurses5-dev
 
 FFMPEG_VERSION=2.3.3
 
